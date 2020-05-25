585ccc9734c4a1bbba620c6957641d5b355a4849
Dimi95 <greek-princess95@hotmail.de>
2020-05-25 22:46:18 +0200

    Fragen geklaert, fehlt noch die Formel

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 const { diamond, edge} = require("./lib/diamond2");
 
 const character = process.argv[2];
-console.log(edge(character))
-console.log(edge(aussenstrich, array [0], aussenstrich));
+console.log(edge(character).join('\n'))
